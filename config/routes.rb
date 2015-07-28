19c8347405c5069eb4a78cf21067ef1d06306463
agara93 <adityagagat@gmail.com>
2015-07-28 10:06:32 +0000

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
     root 'static_pages#home'
     match '/about',     to: 'static_pages#about',   via: 'get'
     
-    resources :users,       only [:new, :create, :show, :index]
+    resources :users,       only: [:new, :create, :show, :index]
     match '/signup',    to: 'users#new',            via: 'get'
     
-    resources :sessions,    only [:new, :create, :destroy]
+    resources :sessions,    only: [:new, :create, :destroy]
     match '/signin',    to: 'sessions#new',         via: 'get'
     match '/signout',   to: 'sessions#destroy',     via: 'delete'
     
+    resources :events,      only: [:new, :create, :show, :index]
+    
+    resources :invites,     only: [:create, :destroy]
 end
