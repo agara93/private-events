c746a04d16f22ffc7f834a9356588c93f548369f
agara93 <adityagagat@gmail.com>
2015-07-29 04:44:16 +0000

    updated static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+      if signed_in?
+          @user = current_user
+          @previous_events = @user.previous_events
+          @upcoming_events = @user.upcoming_events
+      end
   end
 
   def about
