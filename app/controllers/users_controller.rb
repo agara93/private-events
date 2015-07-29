fdd516ba821f776c2a9d37c24738d3173231e418
agara93 <adityagagat@gmail.com>
2015-07-29 04:22:55 +0000

    update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,28 @@ class UsersController < ApplicationController
   end
 
   def create
+      @user = User.new(user_params)
+      if @user.save
+          signin @user
+          redirect_to @user
+      else
+          render 'new'
+      end
   end
 
   def show
+      @user = User.find(params[:id])
+      @previous_events = @user.previous_events
+      @upcoming_events = @user.upcoming_events
+  end
+  
+  def index
+      @users = User.paginate(page: params[:page])
+  end
+  
+  private
+  
+  def user_params
+      params.require(:user).permit(:name, :email)
   end
 end
