e011a41e820eb2d464d41a9638abf82510e128d8
agara93 <adityagagat@gmail.com>
2015-07-29 07:29:00 +0000

    Updated event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-    belongs_to :creator, class_name => "User"
+    belongs_to :creator, :class_name => "User"
     
     has_many :invites, :foreign_key => "attended_events_id"
     has_many :attendees, :through => :invites
